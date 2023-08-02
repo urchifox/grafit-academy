3170d2bd914206a5672fed652ab533284c1428e6
urchifox <126313096+urchifox@users.noreply.github.com>
2023-08-02 21:27:53 +0400

    make packs data and gallery module

---
+++ js/pack-gallery.js
@@ -0,0 +1,34 @@
+import {PREVIEWS_ADRESS} from './packs-data.js';
+
+const root = document.querySelector('.packs-list');
+const template = document.querySelector('#pack-card')
+	.content
+	.querySelector('.pack-card');
+
+const fragment = document.createDocumentFragment();
+
+
+const render = (data) => {
+	data.forEach(({packName, price, coverName, previewsName}, index) => {
+		const card = template.cloneNode(true);
+		card.dataset.id = index;
+		card.querySelector('.pack-card__name-link').textContent = packName;
+		card.querySelector('.pack-card__price').textContent = price;
+		const preview = card.querySelector('.pack-card__cover');
+		preview.src = `${PREVIEWS_ADRESS}${coverName}`;
+		const previewsRoot = card.querySelector('.pack-card__img-link');
+
+		previewsName.forEach((name) => {
+			const newPreview = preview.cloneNode(true);
+			newPreview.src = `${PREVIEWS_ADRESS}${name}`;
+			previewsRoot.append(newPreview);
+		});
+
+		fragment.append(card);
+	});
+
+	root.append(fragment);
+};
+
+
+export {render};
---
+++ js/packs-main.js
@@ -0,0 +1,6 @@
+import {render as renderPacks} from './pack-gallery.js';
+import {packsData} from './packs-data.js';
+import {init as initFavoutite} from './favoutire-buttons.js';
+
+renderPacks(packsData);
+initFavoutite();
