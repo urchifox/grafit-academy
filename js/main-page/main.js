eee513776e6503f2c5674aee3b173fe577bd982d
urchifox <126313096+urchifox@users.noreply.github.com>
2023-11-06 16:05:36 +0400

    change way of workng for let-me-know

--- js/main-page/main.js
+++ js/main-page/main.js
@@ -1,9 +1,7 @@
 import {init as initSlider} from './slider.js';
 import {render as renderPopularPacks} from './popular-packs-rendering.js';
 import {init as initFavoutite} from '../universal/favoutire-buttons.js';
-import {init as initNotifyButtons} from './notify-buttons.js';
 
 initSlider();
 renderPopularPacks();
 initFavoutite();
-initNotifyButtons();
