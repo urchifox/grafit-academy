576e6ab1b0388c0f21db85bee51395f7aa762387
urchifox <126313096+urchifox@users.noreply.github.com>
2023-11-07 22:12:09 +0400

    create disclaimer

--- js/main-page/main.js
+++ js/main-page/main.js
@@ -1,7 +1,9 @@
+import {init as initDisclaimer} from './disclaimer.js';
 import {init as initSlider} from './slider.js';
 import {render as renderPopularPacks} from './popular-packs-rendering.js';
 import {init as initFavoutite} from '../universal/favoutire-buttons.js';
 
+initDisclaimer();
 initSlider();
 renderPopularPacks();
 initFavoutite();
