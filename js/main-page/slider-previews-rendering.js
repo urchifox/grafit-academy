55f19e917682abf6084c72038d060c6e998dee7b
urchifox <126313096+urchifox@users.noreply.github.com>
2023-11-20 16:50:56 +0400

    refactor slider rendering

--- js/main-page/slider-previews-rendering.js
+++ js/main-page/slider-previews-rendering.js
@@ -1,8 +1,16 @@
 import { createElement } from '../utils.js';
 
-const getSliderTemplate = () => /*html*/`
+const getPreviewsTemplate = (data, picturesAddress) => data.map(({src, description}, index) => /*html*/`
+	<li class="slider__preview-item">
+		<button class="slider__preview-btn" data-id="${index}" data-link="" data-desc="">
+			<img class="slider__preview-img" width="100px" height="100px" src="${picturesAddress + src}" alt="${description}">
+		</button>
+	</li>
+`).join('');
+
+const getTemplate = (data, picturesAddress) => /*html*/`
 	<section class="slider">
-		<figcaption class="slider__main-illustration">
+		<figure class="slider__main-illustration">
 			<img class="slider__main-img" src="" alt="">
 			<figcaption class="slider__desc"></figcaption>
 			<button name="previous" aria-label="Предыдущее изображение" class="arrow slider__arrow slider__arrow_left  material-icons-round" >
@@ -11,31 +19,12 @@ const getSliderTemplate = () => /*html*/`
 			<button name="next" aria-label="Следующее изображение" class="arrow slider__arrow slider__arrow_right material-icons-round" >
 				<span aria-hidden="true">navigate_next</span>
 			</button>
-		</figcaption>
+		</figure>
 		<ul class="slider__previews">
+			${getPreviewsTemplate(data, picturesAddress)}
 		</ul>
 	</section>
 `;
-
-const getPreviewsTemplate = (index, picturesAddress, {src, description}) => /*html*/`
-	<li class="slider__preview-item">
-		<button class="slider__preview-btn" data-id="${index}" data-link="" data-desc="" >
-			<img class="slider__preview-img" width="100px" height="100px" src="${picturesAddress + src}" alt="${description}">
-		</button>
-	</li>
-`;
-
-const renderPreviews = (data, picturesAddress) => {
-	const previewContainer = document.querySelector('.slider__previews');
-	const previewFragment = document.createDocumentFragment();
-	data.forEach((datum, index) => {
-		const preview = createElement(getPreviewsTemplate(index, picturesAddress, datum));
-		previewFragment.append(preview);
-	});
-
-	previewFragment.querySelector('.slider__preview-item').classList.add('slider__preview-item_active');
-	previewContainer.append(previewFragment);
-};
 
 const renderMainPicture = (datum, picturesAddress) => {
 	const mainPicture = document.querySelector('.slider__main-img');
@@ -47,9 +36,9 @@ const renderMainPicture = (datum, picturesAddress) => {
 
 const renderSlider = (data, picturesAddress) => {
 	const root = document.querySelector('.slider-container');
-	const slider = createElement(getSliderTemplate());
+	const slider = createElement(getTemplate(data, picturesAddress));
 	root.append(slider);
-	renderPreviews(data, picturesAddress);
+	slider.querySelector('.slider__preview-item').classList.add('slider__preview-item_active');
 	renderMainPicture(data[0], picturesAddress);
 };
 
