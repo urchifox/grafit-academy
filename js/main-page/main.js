e170be5b53a688986e4eb16208bdd27b3b86d3a3
urchifox <126313096+urchifox@users.noreply.github.com>
2023-11-27 13:38:44 +0400

    import cart to main page

--- js/main-page/main.js
+++ js/main-page/main.js
@@ -1,5 +1,6 @@
 import {init as initDisclaimer} from '../universal/disclaimer.js';
 import {init as initAuthorization} from '../universal/authorization.js';
+import {init as initCart} from '../universal/cart.js';
 import {init as initSlider} from './slider.js';
 import {render as renderPopularPacks} from './popular-packs-rendering.js';
 import {init as initScrollButton} from '../universal/scroll-up.js';
@@ -7,6 +8,7 @@ import { ADRESS, studentsWorksInfo } from './students-pictures-data.js';
 
 initDisclaimer();
 initAuthorization();
+initCart();
 initSlider(document.body, studentsWorksInfo, ADRESS);
 renderPopularPacks();
 initScrollButton();
