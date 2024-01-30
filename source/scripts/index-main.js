2ed6f6a1d4f64d5a1fc72bdc5408313d78aa1463
urchifox <126313096+urchifox@users.noreply.github.com>
2024-01-30 21:04:40 +0400

    restructure scripts

--- source/scripts/index-main.js
+++ source/scripts/index-main.js
@@ -1,17 +1,14 @@
 import {init as initDisclaimer} from './disclaimer.js';
 import {init as initAuthorization} from './authorization.js';
 import {init as initCart} from './cart.js';
-import {init as initSorting} from './gallery-manager.js';
-import {init as initScrollButton} from './scroll-up.js';
 import {init as initSlider} from './slider.js';
 import {render as renderPopularPacks} from './popular-packs-rendering.js';
+import {init as initScrollButton} from './scroll-up.js';
 import { ADRESS, studentsWorksInfo } from './students-pictures-data.js';
-import './price-slider.js';
 
 initDisclaimer();
 initAuthorization();
 initCart();
-initSorting();
-initScrollButton();
 initSlider(document.body, studentsWorksInfo, ADRESS);
 renderPopularPacks();
+initScrollButton();
