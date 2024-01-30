df67d98081bae0e868ad62d0639d194bf9da987a
urchifox <126313096+urchifox@users.noreply.github.com>
2024-01-30 15:27:58 +0400

    change favorite button to mask vector

--- source/scripts/favorite-button.js
+++ source/scripts/favorite-button.js
@@ -2,17 +2,8 @@ import { packsUserData } from './packs-user-data.js';
 
 const getTemplate = (id, isFavorite) => /*html*/`
   <label class="favorite-button"  data-id="${id}">
-    <input name="favorite" type="checkbox" class="visually-hidden" ${isFavorite ? 'checked' : ''}/>
+    <input class="favorite-button__checkbox" name="favorite" type="checkbox" ${isFavorite ? 'checked' : ''}/>
     <span class="visually-hidden">Добавить в избранное</span>
-
-    <div class="favorite-button__icon" aria-hidden="true">
-      <span  class="material-icons favorite-button__icon_border">
-        favorite_border
-      </span>
-      <span class="material-icons  favorite-button__icon_checked">
-        favorite
-      </span>
-    </div>
   </label>
 `;
 
