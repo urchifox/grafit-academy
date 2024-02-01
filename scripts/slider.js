79b0da1b84ac9a46ece6a3af35a6af97328a06f8
urchifox <126313096+urchifox@users.noreply.github.com>
2024-02-01 10:08:14 +0000

    Deploying to gh-pages from @ urchifox/grafit-academy@9e4982b6fe9641cbce3e96d4e984e73ac9c3da57 🚀

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,10 +1,10 @@
-function o(t){let e=document.createElement("div");return e.innerHTML=t,e.firstElementChild}var u=(t,e)=>t.map(({src:i,description:r},s)=>`
+function l(r){let e=document.createElement("div");return e.innerHTML=r,e.firstElementChild}var _=(r,e)=>r.map(({src:i,description:t},s)=>`
   <li class="slider__preview-item">
     <button class="slider__preview-btn" data-id="${s}" data-link="" data-desc="">
-      <img class="slider__preview-img" width="100px" height="100px" src="${e+i}" alt="${r}">
+      <img class="slider__preview-img" width="100px" height="100px" src="${e+i}" alt="${t}">
     </button>
   </li>
-`).join(""),p=(t,e)=>`
+`).join(""),u=(r,e)=>`
   <section class="slider">
     <figure class="slider__main-illustration">
       <img class="slider__main-img" src="" alt="">
@@ -17,7 +17,7 @@ function o(t){let e=document.createElement("div");return e.innerHTML=t,e.firstEl
       </button>
     </figure>
     <ul class="slider__previews">
-      ${u(t,e)}
+      ${_(r,e)}
     </ul>
   </section>
-`,c=(t,e,i)=>{let r=t.querySelector(".slider__main-img"),s=t.querySelector(".slider__desc");r.src=i+e.src,s.textContent=e.description},d=(t,e,i)=>{let r=o(p(e,i));t.append(r),r.querySelector(".slider__preview-item").classList.add("slider__preview-item_active"),c(t,e[0],i)};var l=0,_=(t,e)=>{t.previewContainer.querySelector(".slider__preview-item_active").classList.remove("slider__preview-item_active");let r=t.picturesData[e];c(t.container,r,t.picturesAddress),t.previewContainer.children[e].classList.add("slider__preview-item_active"),l=e},m=(t,e)=>{let i=t.target.closest(".slider__arrow");if(!i)return;let r=i.name==="next"?l+1:l-1;r<0&&(r=e.picturesData.length-1),r>e.picturesData.length-1&&(r=0),_(e,r)};function w(t,e){let i=t.target.closest(".slider__preview-btn");if(!i)return;let r=Number(i.dataset.id);_(e,r)}var b=(t,e,i)=>{let r=t.querySelector(".slider-container");d(r,e,i);let s=n=>{let a={root:t,container:r,previewContainer:r.querySelector(".slider__previews"),previousBtn:r.querySelector(".slider__arrow_left"),nextBtn:r.querySelector(".slider__arrow_right"),picturesData:e,picturesAddress:i};n.target.closest(".slider__previews")?w(n,a):m(n,a)};r.addEventListener("click",s)};export{b as init};
+`,c=(r,e,i)=>{let t=r.querySelector(".slider__main-img"),s=r.querySelector(".slider__desc");t.src=i+e.src,s.textContent=e.description},a=(r,e,i)=>{let t=l(u(e,i));r.append(t),t.querySelector(".slider__preview-item").classList.add("slider__preview-item_active"),c(r,e[0],i)};var o=(r,e)=>{r.previewContainer.querySelector(".slider__preview-item_active").classList.remove("slider__preview-item_active");let t=r.picturesData[e];c(r.container,t,r.picturesAddress),r.previewContainer.children[e].classList.add("slider__preview-item_active"),r.currentPreviewIndex=e},p=(r,e)=>{let i=r.target.closest(".slider__arrow");if(!i)return;let t=i.classList.contains("slider__arrow_right")?e.currentPreviewIndex+1:e.currentPreviewIndex-1;t<0&&(t=e.picturesData.length-1),t>e.picturesData.length-1&&(t=0),o(e,t)};function w(r,e){let i=r.target.closest(".slider__preview-btn");if(!i)return;let t=Number(i.dataset.id);o(e,t)}var P=(r,e,i)=>{let t=r.querySelector(".slider-container");a(t,e,i);let s={root:r,container:t,previewContainer:t.querySelector(".slider__previews"),previousBtn:t.querySelector(".slider__arrow_left"),nextBtn:t.querySelector(".slider__arrow_right"),picturesData:e,picturesAddress:i,currentPreviewIndex:0},d=n=>{n.target.closest(".slider__previews")?w(n,s):p(n,s)};t.addEventListener("click",d)};export{P as init};
